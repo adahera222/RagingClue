e51fe80da52baf11ed03f9992936099e71955c2a
Alex Ayars <gitspam@nodekit.org>
2013-08-25 17:49:30 -0700

    Create Clue.cs

---
+++ Clue.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Clue : MonoBehaviour {
+	public string[] clues;
+	public string[] choices;
+	public int correctChoice; // Index of correct answer from choices
+	public string explanation; // Show this on the banter screen
+}
